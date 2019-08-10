22f8831fcdb864017fd58dadf3c0210e01fcb0c3
Samson Monballiu <samsonmonballiu@hotmail.com>
2019-08-10 09:26:37 +0200

    Added Console testing project

---
+++ NoteBunny/NoteBunny.ConsoleTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using NoteBunny.BLL.Models;
+using NoteBunny.DAL.Xml.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBunny.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var notesRepo = new XmlRepository<Note>("notes.xml");
+
+            var note = new Note()
+            {
+                Content = "Hello",
+                Tags = new List<Tag>()
+                {
+                    new Tag()
+                    {
+                        Name = "Unit Testing"
+                    },
+
+                    new Tag()
+                    {
+                        Name = "Doops"
+                    }
+                }
+            };
+
+            notesRepo.Add(note);
+            notesRepo.Save();
+
+            Console.ReadKey();
+        }
+    }
+}
