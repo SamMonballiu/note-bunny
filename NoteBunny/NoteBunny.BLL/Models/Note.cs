e1e9b1bb63f3d6fb902b7db1625285c4586c9e86
Samson Monballiu <samsonmonballiu@hotmail.com>
2019-08-10 11:05:33 +0200

    Added Subject to Note + validation (experimental)

--- NoteBunny/NoteBunny.BLL/Models/Note.cs
+++ NoteBunny/NoteBunny.BLL/Models/Note.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
 
 namespace NoteBunny.BLL.Models
 {
-    public class Note : BaseEntity
+    public class Note : BaseEntity, IValidatableObject
     {
+        public string Subject { get; set; }
         public string Content { get; set; }
+        [XmlIgnore]
         public List<Tag> Tags { get; set; }
         public List<string> TagIds { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var vr = new List<ValidationResult>();
+
+            if (string.IsNullOrEmpty(Content) || string.IsNullOrEmpty(Subject))
+            {
+                vr.Add(new ValidationResult("String and content are required."));
+            }
+
+            return vr;
+        }
     }
 }
