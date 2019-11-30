2ad5e906129b614f2066b44e5e3db3f1d556f4de
Samson Monballiu <samsonmonballiu@hotmail.com>
2019-11-30 10:14:44 +0100

    Added ToDoItem

---
+++ NoteBunny/NoteBunny.BLL/Models/ToDoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NoteBunny.BLL.Models
+{
+    public class ToDoItem : BaseEntity
+    {
+        public string Content { get; set; }
+        public bool IsFinished { get; set; }
+        public DateTime FinishedOn { get; set; }
+    }
+}
