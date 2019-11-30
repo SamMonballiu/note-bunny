291c4d158fe10f434bdf0860f05388a85b616a34
Samson Monballiu <samsonmonballiu@hotmail.com>
2019-11-30 10:22:31 +0100

    Expanded ToDo item

--- NoteBunny/NoteBunny.BLL/Models/ToDoItem.cs
+++ NoteBunny/NoteBunny.BLL/Models/ToDoItem.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Xml.Serialization;
 
 namespace NoteBunny.BLL.Models
 {
     public class ToDoItem : BaseEntity
     {
         public string Content { get; set; }
-        public bool IsFinished { get; set; }
-        public DateTime FinishedOn { get; set; }
+
+        [XmlIgnore, JsonIgnore]
+        public bool IsFinished => FinishedOn.HasValue;
+
+        public DateTime? FinishedOn { get; set; }
+
+        public void Finish()
+        {
+            FinishedOn = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder($"{Content} - Finished: ");
+
+            sb.Append(IsFinished
+                ? FinishedOn.Value.ToShortDateString()
+                : "no") ;
+
+            return sb.ToString();
+        }
     }
 }
